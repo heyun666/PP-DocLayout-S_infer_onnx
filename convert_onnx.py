0faebeb036b9cf8d6e98e65d9e04723d4d7b67eb
heyun666 <61897446+heyun666@users.noreply.github.com>
2025-11-01 17:51:55 +0800

    Update convert_onnx.py

--- convert_onnx.py
+++ convert_onnx.py
@@ -1,26 +1,23 @@
 from paddleocr import LayoutDetection
 import subprocess, os, glob
 
+model_dir="./models"
 # 1. 触发模型下载
-model = LayoutDetection(model_name="PP-DocLayout-S")
+model = LayoutDetection(model_dir=model_dir)
 
-# 2. 自动定位模型目录
-model_dir = os.path.expanduser("~/.paddlex/official_models/PP-DocLayout-S")
-print(f"✅ Model directory confirmed: {model_dir}")
 
 # 3. 自动检测文件名
 pdmodel = glob.glob(os.path.join(model_dir, "*.pdmodel"))[0]
 pdparams = glob.glob(os.path.join(model_dir, "*.pdparams"))[0]
-print(f"Found model file: {os.path.basename(pdmodel)}")
-print(f"Found params file: {os.path.basename(pdparams)}")
 
 # 4. 导出为 ONNX
 onnx_path = "pp_doclayout_s.onnx"
+
 cmd = [
     "paddle2onnx",
     "--model_dir", model_dir,
-    "--model_filename", os.path.basename(pdmodel),
-    "--params_filename", os.path.basename(pdparams),
+    "--model_filename", pdmodel,
+    "--params_filename", pdparams,
     "--save_file", onnx_path,
     "--opset_version", "14",
 ]
