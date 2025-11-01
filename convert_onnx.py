e4cd4592f964a7cfb18474558409d0192ce06c06
heyun666 <61897446+heyun666@users.noreply.github.com>
2025-11-01 18:57:23 +0800

    Update convert_onnx.py

--- convert_onnx.py
+++ convert_onnx.py
@@ -1,25 +1,16 @@
-import os
-import subprocess
+from paddleocr import LayoutDetection
+import subprocess, os
 
-model_dir = "models"
-model_file = "model.pdmodel"
-params_file = "model.pdiparams"
-output_file = "model.onnx"
-
-# 确认模型文件是否存在
-if not os.path.exists(os.path.join(model_dir, model_file)):
-    raise FileNotFoundError(f"{model_file} not found in {model_dir}")
-if not os.path.exists(os.path.join(model_dir, params_file)):
-    raise FileNotFoundError(f"{params_file} not found in {model_dir}")
+# 1. 初始化模型并触发下载
+model = LayoutDetection(model_name="PP-DocLayout-S")
 
 # 构造 paddle2onnx 命令
 cmd = [
     "paddle2onnx",
-    "--model_dir", model_dir,
-    "--model_filename", model_file,
-    "--params_filename", params_file,
-    "--save_file", output_file,
-    "--opset_version", "11",
+    "--model_dir", "/root/.paddlex/official_models/PP-DocLayout-S",
+    "--model_filename", "inference.json",
+    "--params_filename", "inference.pdiparams",
+    "--save_file", "./output/model.onnx",
     "--enable_onnx_checker", "True"
 ]
 
